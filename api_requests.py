9b6fc873252da479cfcf4a7b385d300679f9fd36
azhargmail08 <nur.azhar.adnan@gmail.com>
2025-02-02 04:28:31 +0800

    test

---
+++ api_requests.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://data.gov.my/data-catalogue/births"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Success!")
+    data = response.json()
+else:
+    print("Failed! Status code: ", response.status_code) 
